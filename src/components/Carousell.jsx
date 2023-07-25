6bc0db48519c16f2f0a5711c1ef4aeebf533b8a4
franclobo <fjbl2788@gmail.com>
2023-07-24 22:18:23 -0500

    Add images

---
+++ src/components/Carousell.jsx
@@ -0,0 +1,72 @@
+import Carousel from "react-bootstrap/Carousel";
+import Roldós from "../assets/images/Roldós.jpg";
+import Concejo from "../assets/images/Concejo.jpg";
+
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Roldós}
+          alt="First slide"
+          style={{ height: "500px" }}
+        />
+        <Carousel.Caption>
+          <h3>Parroquia San Judas Tadeo</h3>
+          <p>La Roldós.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Concejo}
+          alt="Second slide"
+          style={{ height: "500px" }}
+        />
+        <Carousel.Caption>
+          <h3>Capilla San Francisco Javier</h3>
+          <p>Concejo Provincial.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Roldós}
+          alt="Third slide"
+          style={{ height: "500px" }}
+        />
+        <Carousel.Caption>
+          <h3>Capilla San José de Cangahua</h3>
+          <p>San José de Cangahua.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Roldós}
+          alt="Fourth slide"
+          style={{ height: "500px" }}
+        />
+        <Carousel.Caption>
+          <h3>Capilla San Antonio de Padua</h3>
+          <p>Catzuqui de Velasco.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Roldós}
+          alt="Fifth slide"
+          style={{ height: "500px" }}
+        />
+        <Carousel.Caption>
+          <h3>Capilla La Hacienda</h3>
+          <p>La hacienda.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
